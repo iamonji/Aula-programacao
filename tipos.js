e202c0ac46b7c8456ac5124d4a228913d473c5d9
Iamonji Nascimento <moon124413@gmail.com>
2024-06-05 12:38:54 -0300

    Aula 2 - Objeto

---
+++ tipos.js
@@ -0,0 +1,9 @@
+let frutas = "manga"
+let legumes = ["alface", "espinafre", "beterraba", "batata"]
+let banana = {
+    "cor": "amarela",
+    "leguminosa": "Não",
+    "fruta": "Sim",
+    "sabor": "doce"
+}
+console.log("Vi na feira bananas, que são da cor " + banana.cor + " e tem o sabor" + " " + banana.sabor + ". São frutas? " + banana.fruta + ". São legumes?" + " " + banana.leguminosa + ".");
